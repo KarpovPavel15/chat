a53a6c90da8103237cfa23d247461f2af79f5e96
DESKTOP-LPU5RVC\Pavel <lichking134@gmail.com>
2018-03-28 01:12:49 +0300

    Create logger and configuration

--- libs/log.js
+++ libs/log.js
@@ -2,7 +2,6 @@
 var winston = require('winston');
 var ENV = process.env.NODE_ENV;
 
-// can be much more flexible than that O_o
 function getLogger(module) {
 
     var path = module.filename.split('/').slice(-2).join('/');
