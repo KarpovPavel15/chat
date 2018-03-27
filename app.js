4adfba6ddfefd146a78eec088210c760abe7449c
DESKTOP-LPU5RVC\Pavel <lichking134@gmail.com>
2018-03-28 01:06:05 +0300

    Create logger and configuration

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 var express = require('express');//connect express
 var http = require('http');//moduls http
 var path = require('path');
+var config=require('config');
 var app = express();//create app
-app.set('port',3000);
+app.set('port',config.get('port'));
 http.createServer(app).listen(app.get('port'), function(){
-    console.log('Express server listening on port ' + app.get('port'));
+    console.log('Express server listening on port ' + config.get('port'));
 });//Express will process all incoming requests
 
 //Middleware
---
+++ config/index.js
@@ -0,0 +1,7 @@
+//configuration port
+var nconf=require('nconf');
+var path=require('path')
+nconf.argv()
+    .env()
+    .file({file: path.join(__dirname,'config.json')});
+module.exports=nconf;
