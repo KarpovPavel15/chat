14c2fa62649f0e5d86e0d49fa981075bfd9de29a
DESKTOP-LPU5RVC\Pavel <lichking134@gmail.com>
2018-03-28 00:39:45 +0300

    Work with handler

--- app.js
+++ app.js
@@ -1,15 +1,54 @@
-
-/**
- * Module dependencies.
- */
-
-var express = require('express');
-var routes = require('./routes');
-var user = require('./routes/user');
-var http = require('http');
+var express = require('express');//connect express
+var http = require('http');//moduls http
 var path = require('path');
+var app = express();//create app
+app.set('port',3000);
+http.createServer(app).listen(app.get('port'), function(){
+    console.log('Express server listening on port ' + app.get('port'));
+});//Express will process all incoming requests
 
-var app = express();
+//Middleware
+app.use(function (req,res,next) {
+    if(req.url=='/'){
+      res.end("Hello");
+    }else{
+      next();
+    }
+});
+app.use(function (req,res,next) {
+    if(req.url=='/test'){
+        res.end("Test");
+    }else{
+        next();
+    }
+});
+
+app.use(function (req,res,next) {
+    if(req.url=="/forbidden"){
+        next(new Error("wops,denied"));
+    }else{
+        next();
+}
+});
+//else
+app.use(function (req,res) {
+    res.send(404,"Page not found");
+});
+//create my handler
+app.use(function (err,req,res,next) {
+    //NODE_ENV='production'
+    if(app.get('env')=='development'){
+       var errorHandler=express.errorHandler();
+       errorHandler(err,req,res,next);
+    }else{
+       res.send(500);
+    }
+});
+/*var routes = require('./routes');
+var user = require('./routes/user');
+
+
+
 
 // all environments
 app.set('port', process.env.PORT || 3000);
@@ -24,14 +63,7 @@ app.use(express.session({ secret: 'your secret here' }));
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
-// development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-}
 
 app.get('/', routes.index);
 app.get('/users', user.list);
-
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
-});
+*/
