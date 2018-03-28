03ce503461bc8e69c1f7cde8dd8556e6fe53f2ee
DESKTOP-LPU5RVC\Pavel <lichking134@gmail.com>
2018-03-28 23:29:26 +0300

    add main environments and create html

--- app.js
+++ app.js
@@ -5,38 +5,26 @@ var config=require('config');
 var log=require('libs/log')(module);
 
 var app = express();//create app
-app.set('port',config.get('port'));
-http.createServer(app).listen(app.get('port'), function(){
-    log.info('Express server listening on port ' + config.get('port'));
-});//Express will process all incoming requests
+app.set('views', path.join(__dirname, '/templates'));
+app.set('view engine', 'ejs');
 
-//Middleware
-app.use(function (req,res,next) {
-    if(req.url=='/'){
-      res.end("Hello");
-    }else{
-      next();
-    }
-});
-app.use(function (req,res,next) {
-    if(req.url=='/test'){
-        res.end("Test");
-    }else{
-        next();
-    }
-});
-
-app.use(function (req,res,next) {
-    if(req.url=="/forbidden"){
-        next(new Error("wops,denied"));
-    }else{
-        next();
+// all environments
+app.use(express.favicon());//read url and give favicon.ico
+if(app.get('env')=='development'){
+    app.use(express.logger('dev'));
+}else{
+    app.use(express.logger('default'))
 }
+app.use(express.bodyParser());//read json(work with get/post):req.body....
+app.use(express.cookieParser());
+app.use(app.router);
+app.get('/',function (req,res,next) {
+   res.render("index",{
+       body:'<b>Hello</b>'
+   });
 });
-//else
-app.use(function (req,res) {
-    res.send(404,"Page not found");
-});
+app.use(express.static(path.join(__dirname, 'public')));
+
 //create my handler
 app.use(function (err,req,res,next) {
     //NODE_ENV='production'
@@ -49,24 +37,9 @@ app.use(function (err,req,res,next) {
 });
 /*var routes = require('./routes');
 var user = require('./routes/user');
-
-
-
-
-// all environments
-app.set('port', process.env.PORT || 3000);
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.methodOverride());
-app.use(express.session({ secret: 'your secret here' }));
-app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
-
-
 app.get('/', routes.index);
 app.get('/users', user.list);
 */
+http.createServer(app).listen(config.get('port'), function(){
+    log.info('Express server listening on port ' + config.get('port'));
+});//Express will process all incoming requests
